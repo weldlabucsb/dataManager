d395ceb430e1ebfc683f1dbf38b7b7f72166f590
mlprichard <41245161+mlprichard@users.noreply.github.com>
2020-06-26 16:02:21 -0700

    Create datetime range
    Using matlabs datetime function makes things nice, learned from pressure plotter

--- ConstructRunList.m
+++ ConstructRunList.m
@@ -16,6 +16,27 @@
 %Strontium Data}"
 starting_directory = "F:\StrontiumData";
 
-initialDir = uigetdir(starting_directory,'Please choose initial data folder');
-finalDir = uigetdir(starting_directory,'Please choose final data folder');
+%get the start and end directory TO THE DAY
+% initialDir = uigetdir(starting_directory,'Please choose initial data folder');
+% finalDir = uigetdir(starting_directory,'Please choose final data folder');
+
+initialDir = 'F:\StrontiumData\2019\2019.12\12.16';
+finalDir = 'F:\StrontiumData\2019\2019.12\12.24';
+
+
+%extract start and end year and month and day
+initYear = str2num(initialDir(length(initialDir)-12:length(initialDir)-9));
+initMonth = str2num(initialDir(length(initialDir)-4:length(initialDir)-3));
+initDay = str2num(initialDir(length(initialDir)-1:length(initialDir)));
+
+finYear = str2num(finalDir(length(finalDir)-12:length(finalDir)-9));
+finMonth = str2num(finalDir(length(finalDir)-4:length(finalDir)-3));
+finDay = str2num(finalDir(length(finalDir)-1:length(finalDir)));
+
+%make datestr object MATLAB (nice because doing +1 increments the day
+%without having to worry about getting months and years right)
+tInit = datetime(initYear,initMonth,initDay);
+tFin = datetime(finYear,finMonth,finDay);
+
+%then, start making the CSV file for the runs that you want to keep. 
 
