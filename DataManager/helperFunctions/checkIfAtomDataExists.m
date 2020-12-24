9b6a441d3bd73bfb53360e03deea7732232a4269
jpagett <pagett.jared@gmail.com>
2020-12-23 16:06:17 -0800

    added boolean output to checkIfAtomDataExists, added semicolons to suppress bool output in RunDataLibrary methods, added .asv to gitignore

--- DataManager/helperFunctions/checkIfAtomDataExists.m
+++ DataManager/helperFunctions/checkIfAtomDataExists.m
@@ -1,9 +1,13 @@
-function checkIfAtomDataExists(dataDir)
+function atomdata_exists = checkIfAtomDataExists(dataDir)
     %Check if the folder for the atom data exists
     if ~exist(dataDir,'dir')
+        atomdata_exists = 0;
         error(strcat('The directory (folder).', dataDir ', does not exist'))
     end
     if ~exist(strcat(dataDir, filesep, 'atomdata.mat'),'file')
+        atomdata_exists = 0;
         error(strcat('No atomdata.mat file exists in the directory ', dataDir))
+    else
+        atomdata_exists = 1;
     end
 end
