606b14e9ae5fe46d410f94988505a4c62af4c59a
jpagett <pagett.jared@gmail.com>
2020-12-23 16:17:37 -0800

    docstring on checkIfAtomDataExistsBoolean

--- DataManager/helperFunctions/checkIfAtomDataExistsBoolean.m
+++ DataManager/helperFunctions/checkIfAtomDataExistsBoolean.m
@@ -1,4 +1,8 @@
 function atomdata_exists = checkIfAtomDataExistsBoolean(dataDir)
+% CHECKIFATOMDATAEXISTSBOOLEAN checks whether the provided path exists. If
+% so, checks whether atomdata.mat exists in that folder. Returns False if
+% folder or atomdata are not found, True if atomdata is found.
+
     %Check if the folder for the atom data exists
     if ~exist(dataDir,'dir')
         atomdata_exists = 0;
